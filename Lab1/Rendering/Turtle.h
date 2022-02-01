d478e8ddae45a255c8cc4c717ff5ad08cad3a0c1
krish khatri <krishkhatri@outlook.com>
2022-01-31 20:08:20 -0600

    Added main

--- Lab1/Rendering/Turtle.h
+++ Lab1/Rendering/Turtle.h
@@ -7,80 +7,124 @@
 
 #define M_PI 3.14159265359
 
-using namespace std; 
+using namespace std;
 
-namespace Turtle{
+namespace Turtle {
 
-class Pt{
-public: 
-	Pt() { x=0;y=0;}
-	double x,y; 
-}; 
+	class Pt {
+	public:
+		Pt() { x = 0; y = 0; }
+		double x, y;
+	};
 
-class Color{
-public: 
-	Color() { r=1;g=1;b=1;}
-	double r,g,b; 
-};
+	class Color {
+	public:
+		Color() { r = 1; g = 1; b = 1; }
+		double r, g, b;
+	};
 
-class Line{
-public: 
-	Line() { width=1;}
-	Pt a,b; 
-	Color c; 
-	double width; 
-}; 
+	class Line {
+	public:
+		Line() { width = 1; }
+		Pt a, b;
+		Color c;
+		double width;
+	};
 
-class TurtleState{
-public: 
-	TurtleState() {	dir = 0; }
-	Pt p; 
-	double dir; // angle in radians
-}; 
+	class TurtleState {
+	public:
+		TurtleState() { dir = 0; }
+		Pt p;
+		double dir; // angle in radians
+	};
 
-// keeps the turtle and a history of its trails 
-class TurtleMachine{
-protected: 
-	TurtleState _turtle; 
-	vector<Line> _trails; 
-	Color _color; 
-	double _width; 
-	double _scale; 
-public: 
-	TurtleMachine(){ reset(); }
+	// keeps the turtle and a history of its trails 
+	class TurtleMachine {
+	protected:
+		TurtleState _turtle;
+		vector<Line> _trails;
+		Color _color;
+		double _width;
+		double _scale;
+	public:
+		TurtleMachine() { reset(); }
 
-	vector<Line>* getLines() { return &_trails; }
+		vector<Line>* getLines() { return &_trails; }
 
-	void reset() { 
-		// fill this in
-	}
+		void reset() {
+			// fill this in
+			_turtle = TurtleState();
+			_trails.clear();
+			_color = Color();
+			_width = 1;
+			_scale = 1;
 
-	void forward(double step) { 
-		// fill this in
-	}
+		}
 
-	void move(double step){
-		// fill this in
-	}
+		void forward(double step) {
+			// fill this in
 
-	void turn(double ang){
-		// fill this in
-	}
+			double dx = cos(_turtle.dir) * step;
+			double dy = sin(_turtle.dir) * step;
 
-	void resize(double s){
-		// fill this in
-	}
+			Line draw;
+			draw.c = _color;
+			draw.width = _width;
 
-	void color(double r, double g, double b){
-		// fill this in
-	}
+			draw.a.x = _turtle.p.x;
+			draw.a.y = _turtle.p.y;
 
-	void lineSize(double size){
-		// fill this in
-	}
+			_turtle.p.x += dx * _scale;
+			_turtle.p.y += dy * _scale;
 
-	Pt pos() { return _turtle.p; }
-}; 
+			draw.b.x = _turtle.p.x;
+			draw.b.y = _turtle.p.y;
+
+			_trails.push_back(draw);
+		}
+
+		void move(double step) {
+			// fill this in
+			double dx = cos(_turtle.dir) * step;
+			double dy = sin(_turtle.dir) * step;
+
+			_turtle.p.x += dx * _scale;
+			_turtle.p.y += dy * _scale;
+		}
+
+		void turn(double ang) {
+			// fill this in
+
+			//double u = cos(_turtle.dir);
+			//double v = sin(_turtle.dir);
+
+			//double newU = u * cos(ang) - v * sin(ang);
+			//double newV = u * sin(ang) + v * cos(ang);
+
+			_turtle.dir += ang;
+
+		}
+
+		void resize(double s) {
+			// fill this in
+			_scale *= s;
+
+		}
+
+		void color(double r, double g, double b) {
+			// fill this in
+			_color.r = r;
+			_color.g = g;
+			_color.b = b;
+		}
+
+		void lineSize(double size) {
+			// fill this in
+			_width = size;
+		}
+
+		Pt pos() { return _turtle.p; }
+	};
 
 }
 #endif
